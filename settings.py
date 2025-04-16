d2a77fb996398d2efd241e8c8e3b0bf75853bff8
Jonathan Adams <3ls-it@pm.me>
2025-04-16 07:03:12 -0400

    Added the use of 'python-settings'. Updated requirements.txt, added settings.py.

---
+++ settings.py
@@ -0,0 +1,6 @@
+# Check the current model choices and choose the one that
+# works for you and with the Assistants API
+MODEL = 'gpt-4.1'
+TEMP = 0.3 # range 0.0 - 2.0
+YOUR_NAME = '' # Your name
+ASSISTANT_NAME = '' # Your assistant's name
