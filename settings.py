7c3b9e8f791e28225542e213d7bb4b5645bb9cb9
Jonathan Adams <3ls-it@pm.me>
2025-05-04 08:43:17 -0400

    Added vector store ID option to settings.py
    On first run, if there is no store ID in settings.py, a new store is
    created and the store ID is displayed. The user is prompted to add
    this store ID to settings.py for future use. Otherwise, a new store
    is created on each run.

--- settings.py
+++ settings.py
@@ -5,3 +5,4 @@ TEMP = 0.3 # range 0.0 - 2.0
 YOUR_NAME = '' # Your name
 ASSISTANT_NAME = '' # Your assistant's name
 OPENAI_API_KEY = 'your-key-goes-here'
+VECTOR_STORE_ID = ''  # ID of the vector store for chat history; set to reuse existing store
